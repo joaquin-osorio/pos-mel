b56986f1039e9aefaaf4dc9e89e0013059b96462
Joaquin Osorio <joaquinosorio55@gmail.com>
2023-01-18 16:34:33 -0300

    feat: improve getRanking endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -7,36 +7,18 @@ import { useEffect, useState } from "react";
 function App() {
   
   const [items, setItems] = useState([]);
-
+  
   useEffect(() => {
-    fetch('/getRanking')
+      fetch('/getRanking')
       .then(res => res.json())
-      .then(res => res.content)
-      .then(res => {
-        const fetchPromises = res.map(element => {
-          return fetch('/getProducts?param=' + element.id)
-            .then(res => res.json())
-            .then(res => {
-              return {
-                id: res.id,
-                position: element.position,
-                title: res.title,
-                seller: res.seller_id
-              }
-            })
-        })
-  
-        Promise.all(fetchPromises)
-          .then(results => setItems(results))
-      })
+      .then(res => setItems(res))
   }, []);
-  
 
   console.log(items);
 
   return (
     <div className="App">
-      <button>Get Ranking</button>
+      <h1>Ranking de productos</h1>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -24,32 +24,22 @@ app.get("/getRanking", (req, res) => {
     .get("https://api.mercadolibre.com/highlights/MLA/category/MLA402916", {
       headers: headers,
     })
-    .then((response) => {
-      res.json(response.data);
+    .then((response) => response.data.content)
+    .then((content) => {
+      const fetchPromises = content.map((element) => {
+        return axios
+          .get(`https://api.mercadolibre.com/items/${element.id}`, {
+            headers: headers,
+          })
+          .then((response) => {
+            return {
+              id: response.data.id,
+              position: element.position,
+              title: response.data.title,
+              seller: response.data.seller_id,
+            };
+          });
+      });
+      Promise.all(fetchPromises).then((results) => res.json(results));
     });
 });
-
-app.get("/getProducts", (req, res) => {
-  axios
-    .get(`https://api.mercadolibre.com/items/${req.query.param}`, {
-      headers: headers,
-    })
-    .then((response) => {
-      res.json(response.data);
-    });
-});
-
-//CODE: TG-63c74c199f137f0001ea02f9-283317126
-
-/*
-
-{
-    "access_token": "APP_USR-147682054087836-011721-45e443e30d33d0219a677896c6e5e55c-283317126",
-    "token_type": "Bearer",
-    "expires_in": 21600,
-    "scope": "offline_access read",
-    "user_id": 283317126,
-    "refresh_token": "TG-63c74debf3db1c0001a176e2-283317126"
-}
-
-*/
